20efebf85095246614e0b6b8ec571a24c478d00a
Angela Diaz <angeldi846@gmail.com>
2022-06-17 23:12:58 -0500

    Adjustment routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-/*Páginas adopción*/
 import Adopcion from './pages/adopcion/Adopcion';
 import Form from './pages/adopcion/Formulario';
 import FormAdoptantes from './pages/adopcion/FormAdoptantes';
@@ -17,14 +16,12 @@ root.render(
   <BrowserRouter>
       <Routes>
 
-        <Route path="/" element={<Adopcion/>}></Route>
-        <Route path='/adopcion'  element={<Navigate replace to={"/"} />}></Route>
+        <Route path='/adopcion'  element={<Adopcion/>}></Route>
         <Route path='/form-adoptantes' element={<FormAdoptantes/>}></Route>
         <Route path='/formulario' element={<Form/>}></Route>
         <Route path='/:id' element={<Mascota/>}></Route>
 
       </Routes>
-    
     </BrowserRouter>
 );
 
