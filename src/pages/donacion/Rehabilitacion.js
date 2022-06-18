fbe1bb6c7a701553ad73e3e28efe65496ede36e0
Alizz M <90586195+alizz3@users.noreply.github.com>
2022-06-18 00:32:56 -0500

    Update Rehabilitacion.js

--- src/pages/donacion/Rehabilitacion.js
+++ src/pages/donacion/Rehabilitacion.js
@@ -6,6 +6,7 @@ import Foto from "../../assets/donacion/img/plan_4rehabilitacion.jpg";
 import '../../css/donacion/Plan.css';
 import Huella from '../../assets/donacion/img/Union.png';
 import Boton from "../../components/donacion/boton/boton";
+import React from 'react';
 
 function  Rehabilitacion() {
     return (  
