8952a5071242a79e57f63544fb34003bf486c211
Alizz M <alizz.mejia03@gmail.com>
2022-06-17 22:07:59 -0500

    Aliz branch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import Adopcion from './pages/adopcion';
+// import Aliados from './pages/aliados';
+// import Blog from './pages/blog';
+// import Donacion from './pages/donacion';
+import Home from './pages/home/Home';
+// import FormularioLogin from './pages/login/FormularioLogin';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      {/* <Route path='/adopcion' element={<Adopcion />}></Route>
+      <Route path='/donacion' element={<Donacion />}></Route>
+      <Route path='/blog' element={<Blog />}></Route>
+      <Route path='/aliados' element={<Aliados />}></Route>
+      <Route path='/formulario' element={<FormularioLogin/>}></Route> */}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Route path='/home' element={<Navigate to={"/"} />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
