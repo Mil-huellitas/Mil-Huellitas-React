0b9218f5d17ecef170581f9c2c9394cc35b8f631
Alizz M <90586195+alizz3@users.noreply.github.com>
2022-06-18 04:10:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import GatosCuidados from '../src/components/blog/navegacion/Gatos/Cuidados';
 import GatosMitos from '../src/components/blog/navegacion/Gatos/Mitos';
 import GatosTamaños from '../src/components/blog/navegacion/Gatos/Tamaños';
 
-import Blog from './pages/blog/Blog'
+
 
 
 
@@ -87,11 +87,11 @@ root.render(
 
      
 
-        <Route path='/blog' element={<Blog />}></Route>
+        
 
         <Route path='/perros/adiest' element={<PerrosAdiest />}></Route>
         <Route path='/perros/cuidados' element={<PerrosCuidados />}></Route>
-        <Route path='/perros/mitos' element={<PerrosMitos />}></Route>
+        <Route path='/blog' element={<PerrosMitos />}></Route>
         <Route path='/perros/tamyrazas' element={<PerrosTamaños />}></Route>
 
         <Route path='/gatos/adiest' element={<GatosAdiest />}></Route>
