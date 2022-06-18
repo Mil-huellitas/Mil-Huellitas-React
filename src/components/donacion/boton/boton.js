17552e9ddee9be2c93b5bce4bdc6ca4966ac22c3
LauraCaipa02 <98822299+LauraCaipa02@users.noreply.github.com>
2022-06-18 01:42:32 -0500

    Update boton.js

--- src/components/donacion/boton/boton.js
+++ src/components/donacion/boton/boton.js
@@ -5,12 +5,12 @@ import React from 'react';
 
 function Boton(btn) {
     return(
-    <section className='botonc'>
-        <div className='btn-container'>
-            <button className='btn_pago'><Link to="/pago" className='btn_pago_link'>{btn.text}</Link></button>
-            <Link className="la_a" to="/pago"><img className='huella_btn' src={Icono} alt=""></img></Link>
+    <section className='botonco'>
+        <div className='btn--container'>
+            <button className='btn__pago'><Link to="/pago" className='btn__pago_link'>{btn.text}</Link></button>
+            <Link className="la__a" to="/pago"><img className='huella__btn' src={Icono} alt=""></img></Link>
         </div>
-        <Link to='/apadrina' className='plan'><i class="fa-solid fa-caret-left"></i>Otros Planes</Link>
+        <Link to='/apadrina' className='plan-l'><i class="fa-solid fa-caret-left"></i>Otros Planes</Link>
     </section>
     );
 }
