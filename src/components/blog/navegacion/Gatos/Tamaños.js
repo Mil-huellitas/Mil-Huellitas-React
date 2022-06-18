545f0fe62492a3649f5404eeae491dd37a32b3ae
Alizz M <90586195+alizz3@users.noreply.github.com>
2022-06-18 03:44:27 -0500

    Update Tamaños.js

--- src/components/blog/navegacion/Gatos/Tamaños.js
+++ src/components/blog/navegacion/Gatos/Tamaños.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from'../../../../components/footer/footer'
-import Header from '../../../../components/header/header'
+import Footer from'../../../../components/footer/Footer'
+import Header from '../../../../components/header/Header'
 
 import '../../../../css/blog/tamaños-g.css';
 import Foto from '../../../../assets/blog/img/blog/istockphoto-1305937455-612x612-PhotoRoom 1.png';
