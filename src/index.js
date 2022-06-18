44ff5c7c70202c005ecede8c2fab918787b171d5
Angela Diaz <angeldi846@gmail.com>
2022-06-17 23:16:12 -0500

    Adjustment routes index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Adopcion from './pages/adopcion/Adopcion';
-import Form from './pages/adopcion/Formulario';
-import FormAdoptantes from './pages/adopcion/FormAdoptantes';
-import Mascota from './pages/adopcion/Mascota';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  <BrowserRouter>
-      <Routes>
-
-        <Route path='/adopcion'  element={<Adopcion/>}></Route>
-        <Route path='/form-adoptantes' element={<FormAdoptantes/>}></Route>
-        <Route path='/formulario' element={<Form/>}></Route>
-        <Route path='/:id' element={<Mascota/>}></Route>
-
-      </Routes>
-    </BrowserRouter>
-);
 
 
